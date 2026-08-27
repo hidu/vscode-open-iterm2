328fb267432541ff7eb7393449b9dd42dc25c493
hidu <duv123+git@gmail.com>
2026-08-27 15:54:05 +0800

    add hot-key

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Our Terminal For VSCode
+VSCode 插件，可以在 vscode 的 `资源管理器` 和 `文件编辑页面` 打开外部终端，并切换到当前所在目录。
 
 ![Our Terminal](res/img1.png)
 
+
+## Thanks
 forked from https://github.com/mushanshitiancai/vscode-open-iterm2
--- src/extension.ts
+++ src/extension.ts
@@ -5,52 +5,49 @@ import * as fs from "fs";
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "extension.our-terminal.open",
-    (e: vscode.Uri) => {
-      if (process.platform === "darwin") {
-        const scriptPath = path.join(__dirname, "../../res/open-item2.scpt");
+  let disposable = vscode.commands.registerCommand("extension.our-terminal.open", (e: vscode.Uri) => {
+    let uri = e;
 
-        console.log(e);
-        fs.stat(e.fsPath, (err, stats) => {
-          if (err) return;
+    if (!uri) {
+      const editor = vscode.window.activeTextEditor;
 
-          let dirPath = e.fsPath;
-          if (stats.isFile()) {
-            dirPath = path.dirname(dirPath);
-          }
-
-          console.log(dirPath);
-          let childProcess = spawn("osascript", [
-            scriptPath,
-            "cd",
-            `"${dirPath}"`,
-          ]);
-        });
-      } else if (process.platform === "win32") {
-        fs.stat(e.fsPath, (err, stats) => {
-          if (err) return;
-
-          let dirPath = e.fsPath;
-
-          if (stats.isFile()) {
-            dirPath = path.dirname(dirPath);
-          }
-
-          // Windows Terminal
-          spawn("wt.exe", ["-d", dirPath], {
-            detached: true,
-            stdio: "ignore",
-          }).unref();
-        });
-      } else {
-        vscode.commands.executeCommand(
-          "workbench.action.terminal.openNativeConsole",
-          e,
-        );
+      if (editor) {
+        uri = editor.document.uri;
       }
-    },
-  );
+    }
+
+    if (!uri) {
+      vscode.window.showWarningMessage("No file or folder selected.");
+      return;
+    }
+
+    const fsPath = uri.fsPath;
+    let dirPath = fsPath;
+
+    try {
+      const stats = fs.statSync(fsPath);
+
+      if (stats.isFile()) {
+        dirPath = path.dirname(fsPath);
+      }
+    } catch {
+      vscode.window.showWarningMessage("Unable to determine the directory.");
+      return;
+    }
+
+    if (process.platform === "darwin") {
+      const scriptPath = path.join(__dirname, "../../res/open-item2.scpt");
+      spawn("osascript", [scriptPath, "cd", `"${dirPath}"`]).unref();
+    } else if (process.platform === "win32") {
+      // Windows Terminal
+      spawn("wt.exe", ["-d", dirPath], {
+        detached: true,
+        stdio: "ignore",
+      }).unref();
+    } else {
+      vscode.commands.executeCommand("workbench.action.terminal.openNativeConsole", e);
+    }
+  });
 
   context.subscriptions.push(disposable);
 }
